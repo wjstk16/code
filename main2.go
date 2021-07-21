fcfe5ff1692750836eab7180ad12f8e908ba8310
Hojoon Won <40791814+wjstk16@users.noreply.github.com>
2021-07-21 17:33:30 +0900

    Update main2.go

--- main2.go
+++ main2.go
@@ -9,7 +9,7 @@ func main() {
   http.HandleFunc("/", hello)
   http.ListenAndServe(":8080", nil)
 }
-
+###
 func hello(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "v0.0.2 : Hello, %s", r.URL.Path[1:])
   fmt.Fprintf(w, "v0.0.2 : Hello, %s", r.URL.Path[1:])

2b77e25cd79e75fa534b564f3b08c862b1cb810b
Hojoon Won <40791814+wjstk16@users.noreply.github.com>
2021-07-21 16:58:01 +0900

    Update main2.go

--- main2.go
+++ main2.go
@@ -12,6 +12,5 @@ func main() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "v0.0.2 : Hello, %s", r.URL.Path[1:])
-  
   fmt.Fprintf(w, "v0.0.2 : Hello, %s", r.URL.Path[1:])
 }
